f9f24f8bd32082f1df97f0e1f9327e6c5e4acdce
Wrehaspati <gedereyna@gmail.com>
2025-01-21 20:10:05 +0800

    feat: make sure user's delete work!

--- src/components/user-alert-dialog.tsx
+++ src/components/user-alert-dialog.tsx
@@ -1,5 +1,22 @@
+"use client";
+
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { useDialog } from "@/hooks/use-dialog";
+import { toast } from "@/hooks/use-toast";
+import axiosInstance from "@/lib/axios";
+
+const confirmDelete = async (id: string) => {
+  axiosInstance.delete('/user/'+id.toString())
+    .then(function (response) {
+      toast({title: response.data?.message})
+    })
+    .catch(function (error) {
+      toast({
+        title: "Failed to submit",
+        description: "Error: " + error + ". " + error?.response?.data?.message,
+      })
+    });
+}
 
 const UserAlertDialog = () => {
   const { dialogs, closeDialog } = useDialog();
@@ -15,7 +32,7 @@ const UserAlertDialog = () => {
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={() => confirmDelete(dialogs["dialogRemoveUser"]?.currentItem?.id)}>Continue</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
